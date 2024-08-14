2efb8403c8bc86a5c7fb05fc64aa2e37b156a8b7
Ibrahem Abdelfatah Gafour <113151426+IbrahemGafour@users.noreply.github.com>
2024-08-14 16:07:57 +0300

    Delete 0x16-api_advanced/main.py

--- 0x16-api_advanced/main.py
+++
@@ -1,8 +0,0 @@
-import praw
-
-reddit = praw.Reddit(
-    client_id="EtKy2mn4FD6NKkArY8QHDA",
-    client_secret="	2mt-qlac7awPB9OZZ3P0cqtC_uPmUw",
-    user_agent="ibrahem_abdelfatah by u/m_runthat",
-    
-)

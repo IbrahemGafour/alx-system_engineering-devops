7378f37fd828af7bbf9ff466d9be80e4e647e8a4
IbrahemGafour <IbrahemGafour>
2024-08-15 17:36:56 +0300

    delay

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,27 @@
-package { 'nginx' :
-    ensur => installed,
+#Time to practice configuring your server with Puppet! Just as 
+#you did before, we’d like you to install and configure an Nginx
+#server using Puppet instead of Bash. To save time and effort, you
+#should also include resources in your manifest to perform a 301
+#redirect when querying /redirect_me.
+
+
+# Install Nginx with puppet
+package { 'nginx':
+  ensure => installed,
 }
 
-file_line { 'install' :
+file_line { 'install':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
-  after  => /listen 80 default_server;',
+  after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-file { '/var/www/html/index.html' :
-  content => 'Hello World',
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
 }
 
-service { 'nginx' :
+service { 'nginx':
   ensure  => running,
-  require => package['nginx'],
-} 
-
+  require => Package['nginx'],
+}
